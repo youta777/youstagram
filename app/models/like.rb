dc1c2bebe25fd3af364f41cb667e28bebc08b94f
youta777 <yousoccer07@yahoo.co.jp>
2020-07-25 04:44:31 +0900

    バグ修正

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,5 +2,5 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :user_id, ununiqueness: { scope: :post_id } # user_idとpost_idの重複禁止
+  validates :user_id, uniqueness: { scope: :post_id } # user_idとpost_idの重複禁止
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i(new create index show destroy) do
     resources :photos, only: %i(create)
-    resources :likes, only: %i(create, destroy)
+    resources :likes, only: %i(create destroy)
   end
 end
